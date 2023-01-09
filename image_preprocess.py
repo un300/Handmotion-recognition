66fe29354976ea0f9a12ed8a47dd1d0677614994
un300 <lan4148@naver.com>
2023-01-09 22:22:26 +0900

    비디오 제너레이터 추가

--- image_preprocess.py
+++ image_preprocess.py
@@ -1,15 +1,19 @@
+import os
 import math
 import cv2
 from PIL import Image as im
-import matplotlib.pyplot as plt
+import torch
 import numpy as np
 import pandas as pd
+
+from torch.utils.data import Dataset, DataLoader
+
 
 def resize_img(src, size=160):
     # 1. resize
     if isinstance(src, str):
         img = np.array(im.open(src).convert('RGB'))
-    elif isinstance(src, np.array):
+    elif isinstance(src, np.ndarray):
         img = src
 
     h, w = img.shape[:2]
@@ -38,4 +42,42 @@ def resize_img(src, size=160):
 
     return img
 
-# %%
+
+class VidoeGenerator(Dataset):
+    def __init__(
+            self,
+            video_folder_path,
+            label_csv_path,
+            fps
+    ):
+        self.video_folder_path = video_folder_path
+
+        indexes = pd.read_csv(
+            label_csv_path,
+            encoding='utf-8-sig'
+        )
+        indexes['file_path'] = [f'{self.video_folder_path}/{path}.mp4' for path in indexes['id']]
+
+        self.indexes = indexes[['file_path', 'label']]
+        self.fps = fps
+
+    def __len__(self):
+        return self.indexes.shape[0]
+
+    def get_video(self, file_path):
+        captures = cv2.VideoCapture(file_path)
+        frames = []
+        for _ in range(self.fps):
+            _, img = captures.read()
+            img = resize_img(img, size=160) / 255
+            frames.extend([img])
+        return torch.FloatTensor(
+            np.array(frames)
+        ).permute(3, 0, 1, 2)
+
+    def __getitem__(self, idx):
+        path, label = self.indexes.iloc[idx, :]
+        frames = self.get_video(path)
+        return frames, label
+
+
